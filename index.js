834005776161b0969a4f1cd65d031c2aaa0d4e5e
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-09-03 19:22:54 +0600

    blog detail api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
@@ -40,6 +40,14 @@ async function run() {
       const result = await blogsCollections.insertOne(blogData);
       res.send(result);
     });
+
+    // get single blog
+    app.get("/blog/:id", async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await blogsCollections.findOne(query)
+      res.send(result)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log(
