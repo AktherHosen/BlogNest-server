a2b326c47f8d4fb7e52deb402c5d9dcf9521b314
Md Akther Hosen <dev.aktherhosen@gmail.com>
2024-09-03 20:12:47 +0600

    blog delete api added

--- index.js
+++ index.js
@@ -68,6 +68,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete blog
+    app.delete("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollections.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
